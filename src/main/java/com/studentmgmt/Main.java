24a35c3fe32386b15981c0bd6f5f7ee45d030254
vinaybadipatla123 <151003792+vinaybadipatla123@users.noreply.github.com>
2026-02-12 16:55:49 +0530

    Created Main.java as the console application entry point

---
+++ src/main/java/com/studentmgmt/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Student Management System!");
+        // Add your application logic here.
+    }
+}
